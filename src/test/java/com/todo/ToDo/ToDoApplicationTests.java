7fbaf3819719c332a677e606a2496589fdb07181
Seetha Ram Rao Vanam <seetharam2602@gmail.com>
2024-12-31 16:38:31 -0600

    configured the API to open index.html by default Removed the contextLoads method from tests file, since build is failing because of that method

--- src/test/java/com/todo/ToDo/ToDoApplicationTests.java
+++ src/test/java/com/todo/ToDo/ToDoApplicationTests.java
@@ -1,13 +1,9 @@
 package com.todo.ToDo;
 
-import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class ToDoApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
 
 }
